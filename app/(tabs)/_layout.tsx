1e964d0b4a149650582db08b9b4e5bae29da0cb7
nouribram <nourtesla@yahoo.com>
2025-06-10 08:22:19 +0300

    n

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,5 +1,7 @@
 import { Tabs } from "expo-router";
 import FontAwesome5 from '@expo/vector-icons/FontAwesome5';
+import AntDesign from '@expo/vector-icons/AntDesign';
+
 
 
 export default function TabsLayout() {
@@ -7,11 +9,16 @@ export default function TabsLayout() {
     <Tabs screenOptions={{ tabBarActiveTintColor: "coral" }}>
        <Tabs.Screen 
        name="index" 
-       options={{ title: "Home",
-        tabBarIcon: () =>  (
-          <FontAwesome5 name="home" size={24} color="black" />
-        ),
-        }} 
+       options={{ 
+        title: "Home",
+        tabBarIcon: ({ color, focused }) => {
+          return focused ? (
+          <FontAwesome5 name="home" size={24} color={color} />
+            ) : (
+          <AntDesign name="home" size={24} color="black"/>        
+          );
+         },
+       }} 
         />
        <Tabs.Screen name="login" options={{ title: "Login"}} />
     </Tabs>
